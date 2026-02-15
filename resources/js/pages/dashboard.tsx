0c0b4bac13123dfb1d17f328e6c90cc832282d81
Marsels <ipb22.m.strikaitis@vtdt.edu.lv>
2026-02-15 17:07:58 +0200

    Seventy-fourth Push

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -57,7 +57,7 @@ export default function Dashboard(props: DashboardProps) {
                 <Head title={t('dashboard.view.title')} />
                 <meta name="description" content="Diagrams, that lets you overview the whole business in one page" />
 
-                <div className="flex flex-col gap-10 overflow-x-auto rounded-xl p-2">
+                <div className="flex flex-col gap-3 md:gap-4 overflow-x-auto rounded-xl p-2">
                     {canAccess && (
                         <BusinessDropdownMenu
                             businesses={props.businesses}
@@ -67,27 +67,27 @@ export default function Dashboard(props: DashboardProps) {
                     )}
 
                     <div className="grid gap-2 md:grid-cols-2">
-                        <div className="relative overflow-hidden rounded-xl border">
+                        <div className="relative h-[25vh] min-h-[220px] max-h-[340px] overflow-hidden rounded-xl border">
                             <DashboardSlot slot="top-left" moduleId={layout['top-left']} setLayout={setLayout}>
                                 {DashboardRenderModule(layout['top-left'], 'top-left')}
                             </DashboardSlot>
                         </div>
 
-                        <div className="relative overflow-hidden rounded-xl border">
+                        <div className="relative h-[25vh] min-h-[220px] max-h-[340px] overflow-hidden rounded-xl border">
                             <DashboardSlot slot="top-right" moduleId={layout['top-right']} setLayout={setLayout}>
                                 {DashboardRenderModule(layout['top-right'], 'top-right')}
                             </DashboardSlot>
                         </div>
                     </div>
 
-                    <div className="grid gap-2 min-h-[100vh] md:min-h-min">
-                        <div className="relative h-auto overflow-hidden rounded-xl border">
+                    <div className="grid gap-2">
+                        <div className="relative h-[29vh] min-h-[260px] max-h-[420px] overflow-hidden rounded-xl border">
                             <DashboardSlot slot="bottom-1" moduleId={layout['bottom-1']} setLayout={setLayout}>
                                 {DashboardRenderModule(layout['bottom-1'], 'bottom-1')}
                             </DashboardSlot>
                         </div>
 
-                        <div className="relative h-auto overflow-hidden rounded-xl border">
+                        <div className="relative h-[29vh] min-h-[260px] max-h-[420px] overflow-hidden rounded-xl border">
                             <DashboardSlot slot="bottom-2" moduleId={layout['bottom-2']} setLayout={setLayout}>
                                 {DashboardRenderModule(layout['bottom-2'], 'bottom-2')}
                             </DashboardSlot>
